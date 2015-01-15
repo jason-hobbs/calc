3acd4ed2f9c0f1eecd08d056597d3097e3961124
Jason Hobbs <grimmstede@feemon.com>
2015-01-14 22:10:36 -0600

    clean up

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def say(msg)
   puts "------------- #{msg} -------------"
 end
@@ -13,12 +11,10 @@ num2 = gets.chomp
 say("1) Add 2) Subtract 3) Multiply 4) Divide")
 operator = gets.chomp
 
-#binding.pry
-
 result = case operator
-when "1" then num1.to_i + num2.to_i
-when "2" then num1.to_i - num2.to_i
-when "3" then num1.to_i * num2.to_i
+  when "1" then num1.to_i + num2.to_i
+  when "2" then num1.to_i - num2.to_i
+  when "3" then num1.to_i * num2.to_i
   else num1.to_f / num2.to_f
 end
 
